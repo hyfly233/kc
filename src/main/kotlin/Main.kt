a0819d5a5be6335192bb1f831ea45fd5cbf13d01
hyfly233 <hyfly233@outlook.com>
2024-08-10 22:37:15 +0800

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,6 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import window.KcMainWindow
 
-
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
